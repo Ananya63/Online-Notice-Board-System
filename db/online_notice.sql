35e9737bbff3334b011688c1d34dccde51117df4
Ananya Saha <79100087+Ananya63@users.noreply.github.com>
2022-11-29 23:50:35 +0530

    Add files via upload

---
+++ db/online_notice.sql
@@ -0,0 +1,100 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Database: `online_notice`
+-- Table structure for table `admin`
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `user`, `pass`) VALUES
+(1, 'admin@gmail.com', 'admin');
+
+-- Table structure for table `notice`
+
+CREATE TABLE `notice` (
+  `notice_id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `Description` text NOT NULL,
+  `Date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table `notice`
+
+INSERT INTO `notice` (`notice_id`, `user`, `subject`, `Description`, `Date`) VALUES
+(1, 'ananya@gmail.com', 'admin', 'developer', '2020-07-02 22:28:14'),
+(2, 'ananya@gmail.com', 'Notice', '1234', '2020-07-02 22:31:13'),
+(3, 'person@gmail.com', 'Notice', '1234', '2020-07-02 22:31:13');
+
+-- Table structure for table `user`
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` char(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `pass` varchar(40) NOT NULL,
+  `mobile` bigint(11) NOT NULL,
+  `gender` varchar(40) NOT NULL,
+  `hobbies` varchar(40) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `dob` datetime NOT NULL,
+  `regid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table `user`
+
+INSERT INTO `user` (`id`, `name`, `email`, `pass`, `mobile`, `gender`, `hobbies`, `image`, `dob`, `regid`) VALUES
+(1, 'Ananya Saha', 'ananya@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 12345, 'f', 'singin', 'person.jpg', '1968-10-17 00:00:00', 2147483647),
+(2, 'Person', 'person@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 999999999, 'm', 'playing', 'person.jpg', '1965-10-18 00:00:00', 2147483647);
+
+-- Indexes for dumped tables
+
+-- Indexes for table `admin`
+
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+-- Indexes for table `notice`
+
+ALTER TABLE `notice`
+  ADD PRIMARY KEY (`notice_id`);
+
+-- Indexes for table `user`
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE `user` ADD FULLTEXT KEY `name` (`name`);
+
+-- AUTO_INCREMENT for dumped tables
+
+-- AUTO_INCREMENT for table `admin`
+
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+-- AUTO_INCREMENT for table `notice`
+
+ALTER TABLE `notice`
+  MODIFY `notice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+-- AUTO_INCREMENT for table `user`
+
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
